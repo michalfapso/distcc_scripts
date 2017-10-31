26e67c9b458b88a21e059768cbffb67190c1c69e
Michal Fapso <michal.fapso@gmail.com>
2017-10-31 11:46:53 +0100

    init

---
+++ distcc_make.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# distcc_make
+#
+# Compile locally when all compile jobs can run simultaneously
+# on the local machine, otherwise submit jobs over the network
+
+TMP_DIR=`mktemp -d /tmp/distcc_make.XXXXXXX`
+mkdir -p $TMP_DIR
+
+#ARGS_WITHOUT_J="`echo "$@" | sed 's/-j[0-9]+//'`"
+ARG_J="`echo $@ | awk '{match($0, /-j([0-9]+)/, a); print a[1];}'`"
+
+time (
+make --just-print $@ > "$TMP_DIR/commands.list"
+)
+JOBS_COUNT=`cat "$TMP_DIR/commands.list" | grep '^distcc' | wc -l`
+CORES_COUNT=`nproc`
+
+echo "CORES_COUNT:$CORES_COUNT JOBS_COUNT:$JOBS_COUNT"
+
+# Determine whether to distribute jobs or run locally
+if [ "$CORES_COUNT" -gt "$JOBS_COUNT" ]; then
+	echo "Run locally"
+	export DISTCC_HOSTS='localhost'
+	if [ -z "$ARG_J" ]; then
+		ARG_J="$CORES_COUNT"
+	fi
+else
+	echo "Submit jobs over network"
+	if [ -z "$ARG_J" ]; then
+		ARG_J="`distcc -j`"
+	fi
+fi
+
+echo "DISTCC_HOSTS:$DISTCC_HOSTS"
+
+
+#if ! type "parallel" > /dev/null; then
+if [ 1 ]; then
+	# If GNU Parallel is not installed, use make instead. This is a bit slower, because make was already executed above and is going to be executed again here
+	make $ARGS_WITHOUT_J -j$ARG_J
+else
+	# echo 'will cite' | parallel --citation
+
+	IFS=$'\n'
+	cat $TMP_DIR/commands.list | awk 'BEGIN{ end = 0 }
+		/^distcc/ {
+			print > "'$TMP_DIR'/commands.list.distcc";
+			end = 1;
+			next
+		}
+		end == 0 {print > "'$TMP_DIR'/commands.list.start"}
+		end == 1 {print > "'$TMP_DIR'/commands.list.end"}'
+
+	# Hack to keep colored gcc output in GNU Parallel
+	if [ "$TERM" == "xterm" ]; then
+		sed -i 's/^\(distcc [^ ]*g++[^ ]*\)\(.*\)/\1 -fdiagnostics-color=always \2/' $TMP_DIR/commands.list.distcc
+	fi
+
+	if [ 1 ]; then
+		echo "start:" ; cat $TMP_DIR/commands.list.start
+		echo "distcc:"; cat $TMP_DIR/commands.list.distcc
+		echo "end:"   ; cat $TMP_DIR/commands.list.end
+		echo ""
+	fi
+
+	# Process serial commands
+	if [ -e "$TMP_DIR/commands.list.start" ]; then
+		for cmd in `cat $TMP_DIR/commands.list.start`; do
+			echo $cmd
+			eval $cmd
+			if [ $? -ne 0 ]; then
+				break
+			fi
+		done
+	fi
+
+	# Process parallel commands
+	cat $TMP_DIR/commands.list.distcc | parallel --max-procs "$ARG_J" --halt now,fail=1 --verbose
+	if [ $? -ne 0 ]; then
+		echo "exit code:$?"
+		echo "exiting make"
+		exit $?
+	fi
+	echo "running parallel jobs...done"
+
+	# Process serial commands
+	for cmd in `cat $TMP_DIR/commands.list.end`; do
+		echo $cmd
+		eval $cmd
+		if [ $? -ne 0 ]; then
+			break
+		fi
+	done
+fi
+
+
+rm -r $TMP_DIR
---
+++ distcc_test.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# distcc_test
+#
+# Periodically checks distcc hosts and updates ~/.distcc/hosts file accordingly.
+# Install the service using ./distcc_test.sh --install-service
+
+if [ "$1" == "--install-service" ]; then
+	cp "$0" "/usr/bin/"
+
+	cygrunsrv.exe -I distccd_test -p /usr/bin/bash.exe -a "`basename $0`" -e PATH=$PATH -e HOME=$HOME
+	if [ $? == 0 ]; then
+		echo "Successfully installed"
+	else
+		echo "Install failed" >&2
+		exit 1
+	fi
+
+	net start distccd_test
+	if [ $? == 0 ]; then
+		echo "Successfully executed"
+	else
+		echo "Execution failed" >&2
+		exit 1
+	fi
+	exit
+fi
+
+TMP_DIR=`mktemp -d /tmp/distcc_test.XXXXXXX`
+mkdir -p $TMP_DIR
+
+cd $TMP_DIR
+
+cat > Makefile << EOF
+CXX = distcc g++
+
+main.exe: main.o
+	\$(CXX) -o \$@ \$^
+
+main.o: main.cpp
+	\$(CXX) -o \$@ -c \$^
+
+.PHONY: clean
+
+clean:
+	rm *.o *.exe
+EOF
+
+cat > main.cpp << EOF
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "main()" << std::endl;
+	return 0;
+}
+EOF
+
+#cat Makefile
+#cat main.cpp
+
+(
+while [ 1 ]; do
+	date +%Y-%m-%d_%H:%M:%S
+
+	(
+	for host in `cat ~/.distcc/hosts.all`; do
+		make clean >/dev/null 2>&1
+		export DISTCC_HOSTS="$host"
+		export DISTCC_VERBOSE=1
+		echo -n "$DISTCC_HOSTS "
+		make main.o 2>&1 \
+			| tee out.log \
+			| awk '
+				#{print}
+				/^distcc.*\(dcc_note_state\) note state 2, file/ { ok=1; }
+				/^distcc.*compiled on/                           { ok=1; }
+				/failed to distribute/                           { ok=0; }
+				/running locally instead/                        { ok=0; }
+				/still in backoff period/                        { ok=0; }
+				END { if(ok) { print "OK" } else { print "BAD" } }
+			'
+
+		#	| grep -E '^distcc.*\(dcc_note_state\) note state 2, file|^distcc.*compiled on|failed to distribute|running locally instead|still in backoff period'
+		#cat out.log
+		#exit
+	done
+	) \
+	| tee /dev/stderr \
+	| awk '$2 == "OK" { print $1 }' \
+	> hosts.tmp
+
+	(
+		local_only=1
+		for local_hostname in "`hostname`" "localhost" "127.0.0.1"; do
+			echo '^'"$local_hostname"'\(/.*\)\?' >> "local_hostnames"
+		done
+
+		if [ ! -z "`cat hosts.tmp | grep -i -v -f local_hostnames`" ]; then
+			# Hosts file should be filled only when there are some non-local hostnames in the list
+			echo "--randomize"
+			cat hosts.tmp
+		fi
+	) > ~/.distcc/hosts
+
+	echo "sleeping..."
+	sleep 5m
+	echo ""
+done
+) 2>&1 | tee /var/log/distcc_test.log
+
+#echo "TMP_DIR:$TMP_DIR"
+rm -r $TMP_DIR
+
