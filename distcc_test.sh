28663b31adfc3f7f9de557333d901fb72887773d
Michal Fapso <michal.fapso@gmail.com>
2018-03-02 13:28:18 +0100

    distcc_test.sh: added PING mode

--- distcc_test.sh
+++ distcc_test.sh
@@ -5,6 +5,9 @@
 # Periodically checks distcc hosts and updates ~/.distcc/hosts file accordingly.
 # Install the service using ./distcc_test.sh --install-service
 
+# DISTCC mode doesn't work reliably as distcc refuses connections when all distcc slots are used on a remote system
+MODE="PING" # PING | DISTCC
+
 if [ "$1" == "--install-service" ]; then
 	cp "$0" "/usr/bin/"
 
@@ -66,21 +69,28 @@ while [ 1 ]; do
 
 	(
 	for host in `cat ~/.distcc/hosts.all`; do
-		make clean >/dev/null 2>&1
-		export DISTCC_HOSTS="$host"
-		export DISTCC_VERBOSE=1
-		echo -n "$DISTCC_HOSTS "
-		make main.o 2>&1 \
-			| tee out.log \
-			| awk '
-				#{print}
-				/^distcc.*\(dcc_note_state\) note state 2, file/ { ok=1; }
-				/^distcc.*compiled on/                           { ok=1; }
-				/failed to distribute/                           { ok=0; }
-				/running locally instead/                        { ok=0; }
-				/still in backoff period/                        { ok=0; }
-				END { if(ok) { print "OK" } else { print "BAD" } }
-			'
+
+		if [ "$MODE" == "PING" ]; then
+			host_onlyname="`echo "$host" | sed 's/[\/,].*//'`"
+			status=`ping -n 1 $host_onlyname | awk '/Received =/ {if ($7=="1,") ok=1} END{if(ok) {print "OK"} else {print "BAD"}}'`
+			echo $host $status
+		else
+			make clean >/dev/null 2>&1
+			export DISTCC_HOSTS="$host"
+			export DISTCC_VERBOSE=1
+			echo -n "$DISTCC_HOSTS "
+			make main.o 2>&1 \
+				| tee out.log \
+				| awk '
+					#{print}
+					/^distcc.*\(dcc_note_state\) note state 2, file/ { ok=1; }
+					/^distcc.*compiled on/                           { ok=1; }
+					/failed to distribute/                           { ok=0; }
+					/running locally instead/                        { ok=0; }
+					/still in backoff period/                        { ok=0; }
+					END { if(ok) { print "OK" } else { print "BAD" } }
+				'
+		fi
 
 		#	| grep -E '^distcc.*\(dcc_note_state\) note state 2, file|^distcc.*compiled on|failed to distribute|running locally instead|still in backoff period'
 		#cat out.log
