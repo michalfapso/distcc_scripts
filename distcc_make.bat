e1ff1b7de5e54221d39c302705880abb99fe66aa
Michal Fapso <michal.fapso@gmail.com>
2018-03-09 14:44:50 +0100

    fix: distcc_make.bat: on some systems variables were not assigned properly maybe because of the inner else-scope?

--- distcc_make.bat
+++ distcc_make.bat
@@ -1,15 +1,16 @@
 @echo off
+
 if not defined MSYS2_DIR (
 	echo Please, define MSYS2_DIR environment variable, e.g. MSYS2_DIR=C:\msys2
 	rem exit /B switch: to exit the current batch script context, and not the command prompt process
 	exit /B 1
-) else (
-	rem set "MSYS2_DIR=C:\SourceCode\msys64"
-	set "MSYS2_DIR_FWD=%MSYS2_DIR:\=/%"
-	rem echo "%MSYS2_DIR%"
-	rem echo "%MSYS2_DIR_FWD%"
-	rem echo "bat args: %*"
-	rem echo "bat pwd: %cd%"
-	set "PWD=%cd:\=/%"
-	"%MSYS2_DIR%"\usr\bin\env MSYSTEM=MINGW64 "%MSYS2_DIR%"\usr\bin\bash -l -c "cd %PWD%; %MSYS2_DIR_FWD%/usr/bin/distcc_make.sh %*"
 )
+
+set "PWD=%cd:\=/%"
+rem echo "PWD: %PWD%"
+
+set "MSYS2_DIR_FWD=%MSYS2_DIR:\=/%"
+rem echo MSYS2_DIR_FWD: %MSYS2_DIR_FWD%
+
+"%MSYS2_DIR%"\usr\bin\env MSYSTEM=MINGW64 "%MSYS2_DIR%"\usr\bin\bash -l -c "cd %PWD%; %MSYS2_DIR_FWD%/usr/bin/distcc_make.sh %*"
+
